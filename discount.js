e9a3cbadd66bfad23ad8d10b42085dc329e93f04
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-06 14:58:19 -0600

    Change arrow function

--- discount.js
+++ discount.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('discount-row').addEventListener('click', (e) => {
+document.addEventListener("DOMContentLoaded", function ()  {
+    document.getElementById('discount-row').addEventListener('click', function (e) {
         if (e.target.classList.contains('discount')) {
             e.preventDefault();
             bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
