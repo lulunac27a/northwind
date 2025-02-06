0763101f6cc0759996765a237fe59f21a1e8e00c
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-06 15:23:31 -0600

    Close toast when Escape key is pressed

--- discount.js
+++ discount.js
@@ -1,8 +1,13 @@
-document.addEventListener("DOMContentLoaded", function ()  {
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('discount-row').addEventListener('click', function (e) {
         if (e.target.classList.contains('discount')) {
             e.preventDefault();
             bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
         }
     });
+    document.getElementById('discount-row').addEventListener('keydown', function (e) {
+        if (e.key == 'Escape') {
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).hide();
+        }
+    })
 });
